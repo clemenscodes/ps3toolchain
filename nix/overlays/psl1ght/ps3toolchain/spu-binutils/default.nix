633012ad33d15d8c5f361537b704050db83c39e3
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-20 21:36:50 +0200

    refactor(spu-binutils): use phases

--- nix/overlays/psl1ght/ps3toolchain/spu-binutils/default.nix
+++ nix/overlays/psl1ght/ps3toolchain/spu-binutils/default.nix
@@ -12,36 +12,39 @@ in
       url = "https://ftp.gnu.org/gnu/binutils/binutils-${version}.tar.bz2";
       sha256 = "sha256-bHr47RyM+bS51ub+CaPh09R5/mOYS6i5smvzVrYxPKk=";
     };
-    phases = "installPhase";
-    installPhase =
-      /*
-      bash
-      */
-      ''
-        mkdir -p $out/build $out/ps3
-        export PS3DEV="$out/ps3"
-        export PSL1GHT="$PS3DEV"
-        cd $out/build
-        ${scripts.copy}/bin/copy_if_not_exists ${src} ${name}.tar.bz2
-        ${scripts.extract}/bin/extract_if_not_exists ${name}.tar.bz2 xvfj binutils-${version}
-        ${scripts.patch}/bin/apply_patch_if_not_applied ${./patches/${pname}-${version}-PS3.patch} binutils-${version}
-        cp ${pkgs.gnu-config}/config.guess ${pkgs.gnu-config}/config.sub binutils-${version}
-        mkdir -p binutils-${version}/build-spu
-        cd binutils-${version}/build-spu
-        ../configure --prefix="$PS3DEV/spu" --target="spu" \
-          --disable-nls \
-          --disable-shared \
-          --disable-debug \
-          --disable-dependency-tracking \
-          --disable-werror \
-          --with-gcc \
-          --with-gnu-as \
-          --with-gnu-ld \
+    unpackPhase = ''
+      export PS3DEV=${placeholder "out"}/ps3
+      export PSL1GHT=$PS3DEV
+      ${scripts.copy}/bin/copy_if_not_exists ${src} ${name}.tar.bz2
+      ${scripts.extract}/bin/extract_if_not_exists ${name}.tar.bz2 xvfj binutils-${version}
+      cp ${pkgs.gnu-config}/config.guess ${pkgs.gnu-config}/config.sub binutils-${version}
+    '';
+    patchPhase = ''
+      ${scripts.patch}/bin/apply_patch_if_not_applied ${./patches/${pname}-${version}-PS3.patch} binutils-${version}
+    '';
+    configurePhase = ''
+      mkdir -p binutils-${version}/build-spu
+      cd binutils-${version}/build-spu
+      ../configure --prefix="$PS3DEV/spu" --target="spu" \
+        --disable-nls \
+        --disable-shared \
+        --disable-debug \
+        --disable-dependency-tracking \
+        --disable-werror \
+        --with-gcc \
+        --with-gnu-as \
+        --with-gnu-ld \
         --enable-lto
-        make -j $(nproc --all 2>&1)
-        make libdir=`pwd`/host-libs/lib install
-        cd $out
-        mv $out/ps3/* $out
-        rm -rf $out/build $out/ps3
-      '';
+    '';
+    buildPhase = ''
+      make -j $(nproc --all 2>&1)
+    '';
+    installPhase = ''
+      mkdir -p $out/build $out/ps3
+      make libdir=`pwd`/host-libs/lib install
+    '';
+    fixupPhase = ''
+      mv $out/ps3/* $out
+      rm -rf $out/build $out/ps3
+    '';
   }
