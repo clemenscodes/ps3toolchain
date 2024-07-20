181f25aec17c79eb1609f97112fd4a586f0ce1ad
Clemens Horn <clemens.horn@mni.thm.de>
2024-07-20 21:48:05 +0200

    refactor(gdb): use phases

--- nix/overlays/psl1ght/ps3toolchain/ppu-gdb/default.nix
+++ nix/overlays/psl1ght/ps3toolchain/ppu-gdb/default.nix
@@ -12,32 +12,34 @@ in
       url = "https://ftp.gnu.org/gnu/gdb/gdb-${version}.tar.xz";
       sha256 = "sha256-HlW0183KezS+EvTOrmUWI6pzsv1kAVIxP59mpxSXV8Q=";
     };
-    phases = "installPhase";
-    installPhase =
-      /*
-      bash
-      */
-      ''
-        mkdir -p $out/build $out/ps3
-        cd $out/build
-        export PS3DEV="$out/ps3"
-        export PSL1GHT="$PS3DEV"
-        ${scripts.copy}/bin/copy_if_not_exists ${src} ${name}.tar.xz
-        ${scripts.extract}/bin/extract_if_not_exists ${name}.tar.xz xvf gdb-${version}
-        ${scripts.patch}/bin/apply_patch_if_not_applied ${./patches/gdb-${version}-PS3.patch} gdb-${version}
-        cp ${pkgs.gnu-config}/config.guess ${pkgs.gnu-config}/config.sub gdb-${version}
-        mkdir -p gdb-${version}/build-ppu
-        cd gdb-${version}/build-ppu
-        ../configure --prefix="$PS3DEV/ppu" --target="powerpc64-ps3-elf" \
-          --disable-multilib \
-          --disable-nls \
-          --disable-sim \
-          --disable-werror
-        PROCS="$(nproc --all 2>&1)" || ret=$?
-        if [ ! -z $ret ]; then PROCS=4; fi
-        make -j $PROCS && make libdir=`pwd`/host-libs/lib install
-        cd $out
-        mv $out/ps3/* $out
-        rm -rf $out/ps3 $out/build
-      '';
+    unpackPhase = ''
+      export PS3DEV=${placeholder "out"}/ps3
+      export PSL1GHT=$PS3DEV
+      ${scripts.copy}/bin/copy_if_not_exists ${src} ${name}.tar.xz
+      ${scripts.extract}/bin/extract_if_not_exists ${name}.tar.xz xvf gdb-${version}
+      cp ${pkgs.gnu-config}/config.guess ${pkgs.gnu-config}/config.sub gdb-${version}
+    '';
+    patchPhase = ''
+      ${scripts.patch}/bin/apply_patch_if_not_applied ${./patches/${pname}-${version}-PS3.patch} gdb-${version}
+    '';
+    configurePhase = ''
+      mkdir -p gdb-${version}/build-ppu
+      cd gdb-${version}/build-ppu
+      ../configure --prefix="$PS3DEV/ppu" --target="powerpc64-ps3-elf" \
+        --disable-multilib \
+        --disable-nls \
+        --disable-sim \
+        --disable-werror
+    '';
+    buildPhase = ''
+      make -j $(nproc --all 2>&1)
+    '';
+    installPhase = ''
+      mkdir -p $out/build $out/ps3
+      make libdir=`pwd`/host-libs/lib install
+    '';
+    fixupPhase = ''
+      mv $out/ps3/* $out
+      rm -rf $out/build $out/ps3
+    '';
   }
